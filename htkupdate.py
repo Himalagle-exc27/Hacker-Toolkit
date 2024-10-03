6d617864b65fe49c6abb31e39c4b46e81e8f2163
Skyhimabruhv27 <slavaseannextgeneration@gmail.com>
2024-10-03 18:08:50 +0800

    Update htkupdate.py

---
+++ htkupdate.py
@@ -0,0 +1,4 @@
+import os
+print "UPDATING..."
+os.system("cd")
+os.system('cd /root/ && rm -fr hackers-tool-kit && git clone https://github.com/Himalagle-exc27/Hacker-Toolkit && echo "[UPDATED]:  Restart Your Terminal"')
